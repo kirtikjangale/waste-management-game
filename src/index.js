cdb3b105e5f216ee6e5631a11b2fa3aeb8fb0ea7
kirtik_jangale <kirtik.jangale2000@gmail.com>
2021-02-02 17:57:33 +0530

    dialog box update

--- src/index.js
+++ src/index.js
@@ -35,6 +35,10 @@ let dialog;
   let text;
 //
 
+const COLOR_PRIMARY = 0x4e342e;
+const COLOR_LIGHT = 0x7b5e57;
+const COLOR_DARK = 0x260e04;
+
 function preload() {
   this.load.image("tiles", "./assets/tilesets/tuxmon-sample-32px-extruded.png");
    // this.load.tilemapTiledJSON("map", "./assets/tilemaps/trial.json");
@@ -196,122 +200,212 @@ function create() {
   })
   .setScrollFactor(0)
   .setDepth(30);
-}
+
+            
+
+}//end create()
 //show(garbage){
 //}
 
 function hitGarbage() {
   collider.active = false;
-  dialog = this.rexUI.add.dialog({
-    x: garbage.x,
-    y: garbage.y,
-    width: 500,
+//   dialog = this.rexUI.add.dialog({
+//     x: garbage.x,
+//     y: garbage.y,
+//     width: 500,
 
-    background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x1565c0),
+//     background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x1565c0),
 
-    title: createLabel(this, 'Title').setDraggable(),
+//     title: createLabel(this, 'Title').setDraggable(),
 
-    toolbar: [
-        createLabel(this, 'O'),
-        createLabel(this, 'X')
-    ],
+//     toolbar: [
+//         createLabel(this, 'O'),
+//         createLabel(this, 'X')
+//     ],
 
-    leftToolbar: [
-        createLabel(this, 'A'),
-        createLabel(this, 'B')
-    ],  
+//     leftToolbar: [
+//         createLabel(this, 'A'),
+//         createLabel(this, 'B')
+//     ],  
 
-    content: createLabel(this, 'Content'),
+//     content: createLabel(this, 'Content'),
 
-    description: createLabel(this, 'Description'),
+//     description: createLabel(this, 'Description'),
 
-    choices: [
-        createLabel(this, 'Choice0'),
-        createLabel(this, 'Choice1'),
-        createLabel(this, 'Choice2')
-    ],
+//     choices: [
+//         createLabel(this, 'Choice0'),
+//         createLabel(this, 'Choice1'),
+//         createLabel(this, 'Choice2')
+//     ],
 
-    actions: [
-        createLabel(this, 'Action0'),
-        createLabel(this, 'Action1')
-    ],
+//     actions: [
+//         createLabel(this, 'Action0'),
+//         createLabel(this, 'Action1')
+//     ],
 
-    space: {
-        left: 20,
-        right: 20,
-        top: -20,
-        bottom: -20,
+//     space: {
+//         left: 20,
+//         right: 20,
+//         top: -20,
+//         bottom: -20,
 
-        title: 25,
-        titleLeft: 30,
-        content: 25,
-        description: 25,
-        descriptionLeft: 20,
-        descriptionRight: 20,
-        choices: 25,
+//         title: 25,
+//         titleLeft: 30,
+//         content: 25,
+//         description: 25,
+//         descriptionLeft: 20,
+//         descriptionRight: 20,
+//         choices: 25,
 
-        toolbarItem: 5,
-        choice: 15,
-        action: 15,
-    },
+//         toolbarItem: 5,
+//         choice: 15,
+//         action: 15,
+//     },
 
-    expand: {
-        title: false,
-        // content: false,
-        // description: false,
-        // choices: false,
-        // actions: true,
-    },
+//     expand: {
+//         title: false,
+//         // content: false,
+//         // description: false,
+//         // choices: false,
+//         // actions: true,
+//     },
 
-    align: {
-        title: 'center',
-        // content: 'left',
-        // description: 'left',
-        // choices: 'left',
-        actions: 'right', // 'center'|'left'|'right'
-    },
+//     align: {
+//         title: 'center',
+//         // content: 'left',
+//         // description: 'left',
+//         // choices: 'left',
+//         actions: 'right', // 'center'|'left'|'right'
+//     },
 
-    click: {
-        mode: 'release'
-    }
+//     click: {
+//         mode: 'release'
+//     }
+//   })
+//   .setDraggable('background')   // Draggable-background
+//   .layout()
+// // .drawBounds(this.add.graphics(), 0xff0000)
+//   .popUp(1000);
+
+//   var tween = this.tweens.add({
+//     targets: dialog,
+//     scaleX: 1,
+//     scaleY: 1,
+//     ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+//     duration: 1000,
+//     repeat: 0, // -1: infinity
+//     yoyo: false
+// });
+
+//   this.print = this.add.text(0, 0, '');
+//   dialog
+//     .on('button.click', function (button, groupName, index, pointer, event) {
+//         this.print.text += groupName + '-' + index + ': ' + button.text + '\n';
+//     }, this)
+//     .on('button.over', function (button, groupName, index, pointer, event) {
+//         button.getElement('background').setStrokeStyle(1, 0xffffff);
+//     })
+//     .on('button.out', function (button, groupName, index, pointer, event) {
+//         button.getElement('background').setStrokeStyle();
+//     });
+var data = {
+  name: 'Rex',
+  skills: [
+      { name: 'A' },
+      { name: 'B' },
+      { name: 'C' },
+      { name: 'D' },
+      { name: 'E' },
+  ],
+  items: [
+      { name: 'A' },
+      { name: 'B' },
+      { name: 'C' },
+      { name: 'D' },
+      { name: 'E' },
+      { name: 'F' },
+      { name: 'G' },
+      { name: 'H' },
+      { name: 'I' },
+      { name: 'J' },
+      { name: 'K' },
+      { name: 'L' },
+      { name: 'M' },
+  ],
+
+};
+
+var scrollablePanel = this.rexUI.add.scrollablePanel({
+      x: 400,
+      y: 300,
+      width: 400,
+      height: 220,
+
+      scrollMode: 1,
+
+      background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 10, COLOR_PRIMARY),
+
+      panel: {
+          child: createPanel(this, data),
+
+          mask: {
+              padding: 1
+          },
+      },
+
+      slider: {
+          track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, COLOR_DARK),
+          thumb: this.rexUI.add.roundRectangle(0, 0, 0, 0, 13, COLOR_LIGHT),
+      },
+
+      // scroller: true,
+
+      space: {
+          left: 10,
+          right: 10,
+          top: 10,
+          bottom: 10,
+
+          panel: 10,
+      }
   })
-  .setDraggable('background')   // Draggable-background
-  .layout()
-// .drawBounds(this.add.graphics(), 0xff0000)
-  .popUp(1000);
+  .layout().setScrollFactor(0).setDepth(30)
+//.drawBounds(this.add.graphics(), 0xff0000);
 
-  var tween = this.tweens.add({
-    targets: dialog,
-    scaleX: 1,
-    scaleY: 1,
-    ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
-    duration: 1000,
-    repeat: 0, // -1: infinity
-    yoyo: false
-});
+// Set icon interactive
 
-  this.print = this.add.text(0, 0, '');
-  dialog
-    .on('button.click', function (button, groupName, index, pointer, event) {
-        this.print.text += groupName + '-' + index + ': ' + button.text + '\n';
-    }, this)
-    .on('button.over', function (button, groupName, index, pointer, event) {
-        button.getElement('background').setStrokeStyle(1, 0xffffff);
-    })
-    .on('button.out', function (button, groupName, index, pointer, event) {
-        button.getElement('background').setStrokeStyle();
-    });
+this.input.topOnly = false;
+var labels = [];
+labels.push(...scrollablePanel.getElement('#skills.items', true));
+labels.push(...scrollablePanel.getElement('#items.items', true));
+var scene = this;
+labels.forEach(function (label) {
+  if (!label) {
+      return;
+  }
+
+  var click = scene.rexUI.add.click(label.getElement('icon'), { threshold: 10 })
+      .on('click', function () {
+          if (!label.getTopmostSizer().isInTouching()) {
+              return;
+          }
+          var category = label.getParentSizer().name;
+          score+=1;
+          console.log(`${category}:${label.text}\n`)
+          text.setText(`Score:${score}`)
+      });
+})
   
 }
 
 var createLabel = function (scene, text) {
   return scene.rexUI.add.label({
-      width: 40, // Minimum width of round-rectangle
-      height: 40, // Minimum height of round-rectangle
+      width: 20, // Minimum width of round-rectangle
+      height: 20, // Minimum height of round-rectangle
     
-      background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x5e92f3),
+      background: scene.add.image(0,0,'garbage').setScrollFactor(0).setDepth(30).setScale(0.1,1),
 
-      text: scene.add.text(0, 0, text, {
+      text: scene.add.text(0, 0, 'vdlf', {
           fontSize: '24px'
       }),
 
@@ -402,3 +496,120 @@ function update(time, delta) {
     // text.setText(`Score:${score}`);
   }
 }
+
+var createPanel = function (scene, data) {
+  var sizer = scene.rexUI.add.sizer({
+      orientation: 'x',
+      space: { item: 10 }
+  })
+      .add(
+          createHeader(scene, data), // child
+          { expand: true }
+      )
+      .add(
+          createTable(scene, data, 'skills', 1), // child
+          { expand: true }
+      )
+      .add(
+          createTable(scene, data, 'items', 2), // child
+          { expand: true }
+      )
+  return sizer;
+}
+
+var createHeader = function (scene, data) {
+  var title = scene.rexUI.add.label({
+      orientation: 'x',
+      text: scene.add.text(0, 0, 'Character'),
+  });
+  var header = scene.rexUI.add.label({
+      orientation: 'y',
+      icon: scene.rexUI.add.roundRectangle(0, 0, 100, 100, 5, COLOR_LIGHT),
+      text: scene.add.text(0, 0, data.name),
+
+      space: { icon: 10 }
+  });
+
+  return scene.rexUI.add.sizer({
+      orientation: 'y',
+      space: { left: 5, right: 5, top: 5, bottom: 5, item: 10 }
+  })
+      .addBackground(
+          scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(2, COLOR_LIGHT, 1)
+      )
+      .add(
+          title, // child
+          { expand: true, align: 'left' }
+      )
+      .add(header, // child
+          { proportion: 1, expand: true }
+      );
+};
+
+var createTable = function (scene, data, key, rows) {
+  var capKey = key.charAt(0).toUpperCase() + key.slice(1);
+  var title = scene.rexUI.add.label({
+      orientation: 'x',
+      text: scene.add.text(0, 0, capKey),
+  });
+
+  var items = data[key];
+  var columns = Math.ceil(items.length / rows);
+  var table = scene.rexUI.add.gridSizer({
+      column: columns,
+      row: rows,
+
+      rowProportions: 1,
+      space: { column: 10, row: 10 },
+      name: key  // Search this name to get table back
+  });
+
+  var item, r, c;
+  var iconSize = (rows === 1) ? 80 : 40;
+  for (var i = 0, cnt = items.length; i < cnt; i++) {
+      item = items[i];
+      r = i % rows;
+      c = (i - r) / rows;
+      table.add(
+          createIcon(scene, item, iconSize, iconSize),
+          c,
+          r,
+          'top',
+          0,
+          true
+      );
+  }
+
+  return scene.rexUI.add.sizer({
+      orientation: 'y',
+      space: { left: 10, right: 10, top: 10, bottom: 10, item: 10 }
+  })
+      .addBackground(
+          scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(2, COLOR_LIGHT, 1)
+      )
+      .add(
+          title, // child
+          0, // proportion
+          'left', // align
+          0, // paddingConfig
+          true // expand
+      )
+      .add(table, // child
+          1, // proportion
+          'center', // align
+          0, // paddingConfig
+          true // expand
+      );
+}
+
+var createIcon = function (scene, item, iconWidth, iconHeight) {
+  var label = scene.rexUI.add.label({
+      orientation: 'y',
+      icon: scene.add.image(0,0,'garbage').setScrollFactor(0).setDepth(30).setScale(0.3,0.4),
+      text: scene.add.text(0, 0, item.name),
+
+      space: { icon: 3 }
+  });
+
+  return label;
+};
