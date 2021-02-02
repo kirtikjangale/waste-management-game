0429b83af3c1022a2ac5e82768b5e198cf273cd3
kirtik_jangale <kirtik.jangale2000@gmail.com>
2021-02-02 12:59:08 +0530

    checkpoint

--- src/scenes/gamescence.js
+++ src/scenes/gamescence.js
@@ -10,7 +10,7 @@ let collider;
 //
   let score=0;
   let text;
-  let dialog;
+  let dialog = undefined;
 
 var config = {
     key: 'GameScene',
@@ -98,18 +98,7 @@ class GameScene extends Phaser.Scene {
 
       
     
-      collider = this.physics.add.overlap(player, zone, function (zone,player){
-        collider.active = false;
-
-       
-        
-        // else if (!dialog.isInTouching(pointer)) {
-        //     dialog.scaleDownDestroy(100);
-        //     dialog = undefined;
-        //     console.log('dsfsssasja')
-        // }
-        // hitGarbage(garbage);
-      }, null, this);
+      this.collider = this.physics.add.overlap(player, zone, this.hitGarbage(zone), null, this);
     
       // this.physics.add.collider(player, garbage, hitGarbage, null, this);
       // // Create the player's walking animations from the texture atlas. These are stored in the global
@@ -225,51 +214,7 @@ class GameScene extends Phaser.Scene {
     }
     update() {
 
-      score = score+0.1;
-      var overlap = this.checkOverlap(player, zone);
-      
-      if (!(overlap.width===0 && overlap.height===0))
-      {   
-          collider.active = false;
-          var scene = this
-          
-          if(dialog===undefined){
-            this.input.on('pointerdown', function (pointer) {
-              var x = pointer.x,
-                  y = pointer.y;
-
-              if (dialog === undefined) {
-                  dialog = this.createDialog(this, x, y, function (color) {
-                      scene.add.circle(x, y, 20, color).setScrollFactor(0).setDepth(30);;
-                    // scene.print.text = 'Add object at (' + x + ',' + y + ')';
-                      dialog.scaleDownDestroy(100);
-                      dialog = undefined;
-                      
-                  });
-                  //scene.print.text = 'Click (' + x + ',' + y + ')';
-              } else if (!dialog.isInTouching(pointer)) {
-                  dialog.scaleDownDestroy(100);
-                  dialog = undefined;
-                  console.log('dsfsssasja')
-              }
-            }, this);
-          }
-          
-      }
-      else
-      {   
-        collider.active = true;
-        
-        console.log('gi')
-        
-        if(dialog!==undefined){
-          dialog.scaleDownDestroy(100);
-          dialog = undefined;
-
-          
-        }
-      
-      }
+     
       
       const speed = 500;
       const prevVelocity = player.body.velocity.clone();
@@ -303,17 +248,32 @@ class GameScene extends Phaser.Scene {
       } else if (cursors.down.isDown) {
         player.anims.play("misa-front-walk", true);
       } else {
-        player.anims.stop();
-    
-        // If we were moving, pick and idle frame to use
-        if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
-        else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
-        else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
-        else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
-    
-       
-       
-      }
+
+          player.anims.stop();
+      
+          // If we were moving, pick and idle frame to use
+          if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+          else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+          else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+          else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+      
+          score = score+0.1;
+          var overlap = this.checkOverlap(player, zone);
+          
+          if (!(overlap.width===0 && overlap.height===0))
+          {   
+              collider.active = false;
+              
+              
+              
+          }
+          else
+          {   
+            collider.active = true;
+            console.log(dialog)
+          }
+        
+        }
 
       
 
@@ -322,67 +282,67 @@ class GameScene extends Phaser.Scene {
 
     
 
-    createDialog(scene, x, y, onClick) {
-      var dialog = scene.rexUI.add.dialog({
-              x: x,
-              y: y,
+    // createDialog(scene, x, y, onClick) {
+    //   var dialog = scene.rexUI.add.dialog({
+    //           x: x,
+    //           y: y,
     
-              background: scene.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0xf57f17).setScrollFactor(0).setDepth(30),
+    //           background: scene.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0xf57f17).setScrollFactor(0).setDepth(30),
     
 
-              title: scene.rexUI.add.label({
-                  background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0xbc5100).setScrollFactor(0).setDepth(30),
-                  text: scene.add.text(0, 0, 'Pick a color', {
-                      fontSize: '20px'
-                  }),
-                  space: {
-                      left: 15,
-                      right: 15,
-                      top: 10,
-                      bottom: 10
-                  }
-              }),
+    //           title: scene.rexUI.add.label({
+    //               background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0xbc5100).setScrollFactor(0).setDepth(30),
+    //               text: scene.add.text(0, 0, 'Pick a color', {
+    //                   fontSize: '20px'
+    //               }),
+    //               space: {
+    //                   left: 15,
+    //                   right: 15,
+    //                   top: 10,
+    //                   bottom: 10
+    //               }
+    //           }),
     
-              actions: [
-                  scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0xe91e63).setScrollFactor(0).setDepth(30),
-                  scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x673ab7).setScrollFactor(0).setDepth(30),
-                  scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x2196f3).setScrollFactor(0).setDepth(30),
-                  scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x00bcd4).setScrollFactor(0).setDepth(30),
-                  scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x4caf50).setScrollFactor(0).setDepth(30),
-                  scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0xcddc39).setScrollFactor(0).setDepth(30),
-              ],
+    //           actions: [
+    //               scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0xe91e63).setScrollFactor(0).setDepth(30),
+    //               scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x673ab7).setScrollFactor(0).setDepth(30),
+    //               scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x2196f3).setScrollFactor(0).setDepth(30),
+    //               scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x00bcd4).setScrollFactor(0).setDepth(30),
+    //               scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x4caf50).setScrollFactor(0).setDepth(30),
+    //               scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0xcddc39).setScrollFactor(0).setDepth(30),
+    //           ],
     
-              actionsAlign: 'left',
+    //           actionsAlign: 'left',
     
-              space: {
-                  title: 10,
-                  action: 5,
+    //           space: {
+    //               title: 10,
+    //               action: 5,
     
-                  left: 10,
-                  right: 10,
-                  top: 10,
-                  bottom: 10,
-              }
-          })
-          .layout()
-          .pushIntoBounds()
-          //.drawBounds(this.add.graphics(), 0xff0000)
-          .popUp(500).setScrollFactor(0).setDepth(30);
+    //               left: 10,
+    //               right: 10,
+    //               top: 10,
+    //               bottom: 10,
+    //           }
+    //       })
+    //       .layout()
+    //       .pushIntoBounds()
+    //       //.drawBounds(this.add.graphics(), 0xff0000)
+    //       .popUp(500).setScrollFactor(0).setDepth(30);
     
-      dialog
-          .on('button.click', function (button, groupName, index) {
-              onClick(button.fillColor);
-          })
-          .on('button.over', function (button, groupName, index) {
-              button.setStrokeStyle(2, 0xffffff);
-          })
-          .on('button.out', function (button, groupName, index) {
-              button.setStrokeStyle();
-          });
+    //   dialog
+    //       .on('button.click', function (button, groupName, index) {
+    //           onClick(button.fillColor);
+    //       })
+    //       .on('button.over', function (button, groupName, index) {
+    //           button.setStrokeStyle(2, 0xffffff);
+    //       })
+    //       .on('button.out', function (button, groupName, index) {
+    //           button.setStrokeStyle();
+    //       });
     
        
-      return dialog;
-    }
+    //   return dialog;
+    // }
 
     checkOverlap(spriteA, spriteB) {
 
@@ -391,6 +351,130 @@ class GameScene extends Phaser.Scene {
     
       return Phaser.Geom.Rectangle.Intersection(boundsA, boundsB);
     
+    }
+
+    hitGarbage(garbage) {
+      this.collider.active = false
+      var dialog = this.rexUI.add.dialog({
+        x: garbage.x,
+        y: garbage.y,
+        width: 500,
+    
+        background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x1565c0),
+    
+        title: this.createLabel(this, 'Title').setDraggable(),
+    
+        toolbar: [
+          this.createLabel(this, 'O'),
+            this.createLabel(this, 'X')
+        ],
+    
+        leftToolbar: [
+            this.createLabel(this, 'A'),
+            this.createLabel(this, 'B')
+        ],  
+    
+        content: this.createLabel(this, 'Content'),
+    
+        description: this.createLabel(this, 'Description'),
+    
+        choices: [
+            this.createLabel(this, 'Choice0'),
+            this.createLabel(this, 'Choice1'),
+            this.createLabel(this, 'Choice2')
+        ],
+    
+        actions: [
+            this.createLabel(this, 'Action0'),
+            this.createLabel(this, 'Action1')
+        ],
+    
+        space: {
+            left: 20,
+            right: 20,
+            top: -20,
+            bottom: -20,
+    
+            title: 25,
+            titleLeft: 30,
+            content: 25,
+            description: 25,
+            descriptionLeft: 20,
+            descriptionRight: 20,
+            choices: 25,
+    
+            toolbarItem: 5,
+            choice: 15,
+            action: 15,
+        },
+    
+        expand: {
+            title: false,
+            // content: false,
+            // description: false,
+            // choices: false,
+            // actions: true,
+        },
+    
+        align: {
+            title: 'center',
+            // content: 'left',
+            // description: 'left',
+            // choices: 'left',
+            actions: 'right', // 'center'|'left'|'right'
+        },
+    
+        click: {
+            mode: 'release'
+        }
+      })
+      .setDraggable('background')   // Draggable-background
+      .layout()
+    // .drawBounds(this.add.graphics(), 0xff0000)
+      .popUp(1000);
+    
+      var tween = this.tweens.add({
+        targets: dialog,
+        scaleX: 1,
+        scaleY: 1,
+        ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+        duration: 1000,
+        repeat: 0, // -1: infinity
+        yoyo: false
+    });
+    
+      this.print = this.add.text(0, 0, '');
+      dialog
+        .on('button.click', function (button, groupName, index, pointer, event) {
+            this.print.text += groupName + '-' + index + ': ' + button.text + '\n';
+        }, this)
+        .on('button.over', function (button, groupName, index, pointer, event) {
+            button.getElement('background').setStrokeStyle(1, 0xffffff);
+        })
+        .on('button.out', function (button, groupName, index, pointer, event) {
+            button.getElement('background').setStrokeStyle();
+        });
+      console.log("player collided with garbage!");
+      
+    }
+    createLabel(scene, text) {
+      return scene.rexUI.add.label({
+          width: 40, // Minimum width of round-rectangle
+          height: 40, // Minimum height of round-rectangle
+        
+          background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x5e92f3),
+    
+          text: scene.add.text(0, 0, text, {
+              fontSize: '24px'
+          }),
+    
+          space: {
+              left: 10,
+              right: 10,
+              top: 10,
+              bottom: 10
+          }
+      });
     }
 }
 
